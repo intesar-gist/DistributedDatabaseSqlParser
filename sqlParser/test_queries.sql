46b1329b3ea43693a28dd998a2bf5c994e0c4df8
Intesar Haider <intesar.haider1@gmail.com>
2017-08-22 17:56:15 +0200

    QUERIES UPDATED

--- sqlParser/test_queries.sql
+++ sqlParser/test_queries.sql
@@ -44,17 +44,16 @@ FHC		varchar(3),
 LAND		varchar(3),
 STADT		varchar(50) ,
 NAME		varchar(50) ,
-constraint FLUGHAFEN_PS
-		primary key (FHC),
+constraint FLUGHAFEN_PS primary key (FHC),
 );
 
 create table FLUG (
-FNR             integer,
+FNR    integer,
 FLC		varchar(2),
 FLNR		integer,
 VON		varchar(3),
 NACH		varchar(3),
-AB		integer,
+AB		integer(77),
 AN		integer,
 constraint FLUG_PS
 		primary key (FNR),
